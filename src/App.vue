2b8990ca7c11046191ce9aa8998dcc753ae9082c
Shynu Mary Varghese <65387041+estrelabrilante@users.noreply.github.com>
2022-02-01 20:15:17 +0530

    introducing mutations

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <base-container title="Vuex">
+    <the-counter></the-counter>
+    <button @click="addOne">Add 1</button>
+    <the-change-counter></the-change-counter>
+  </base-container>
+</template>
+
+<script>
+import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import TheChangeCounter from './components/TheChangeCounter.vue';
+
+export default {
+  components: {
+    BaseContainer,
+    TheCounter,
+    TheChangeCounter,
+  },
+
+  methods: {
+    addOne() {
+      // this.$store.state.counter++;
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,21 @@
+import { createApp } from 'vue';
+import { createStore } from 'vuex';
+
+import App from './App.vue';
+
+const store = createStore({
+  state() {
+    return {
+      counter: 0,
+    };
+  },
+  mutations: {
+    increment(state) {
+      state.counter = state.counter + 1;
+    },
+  },
+});
+const app = createApp(App);
+// connecting app to store
+app.use(store);
+app.mount('#app');
